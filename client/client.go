0cb3fb669757c035e645d129111177eb8d8245d6
lmm1ng <dei4@yandex.ru>
2025-08-10 02:08:42 +0300

    feat: add client

---
+++ client/client.go
@@ -0,0 +1,76 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/eigakan/nats-shared/model"
+	"github.com/nats-io/nats.go"
+)
+
+type Client struct {
+	nc *nats.Conn
+}
+
+func NewClient(host string, port int) (*Client, error) {
+	nc, err := nats.Connect(fmt.Sprintf("nats://%s:%d", host, port))
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to NATS server: %w", err)
+	}
+	return &Client{
+		nc: nc,
+	}, nil
+}
+
+func (c *Client) Close() error {
+	if c.nc == nil {
+		return fmt.Errorf("NATS connection is nil")
+	}
+
+	c.nc.Close()
+	return nil
+}
+
+func (c *Client) Drain() error {
+	if c.nc == nil {
+		return fmt.Errorf("NATS connection is nil")
+	}
+
+	return c.nc.Drain()
+}
+
+func (c *Client) RespondErr(msg *nats.Msg, errText string) error {
+	var resDto model.NatsResponse[any]
+	resDto.Err = errText
+
+	res, err := json.Marshal(resDto)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal error response: %w", err)
+	}
+
+	err = msg.Respond(res)
+
+	if err != nil {
+		return fmt.Errorf("Failed to send error response via nats: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) Respond(msg *nats.Msg, data any) error {
+	var resDto model.NatsResponse[any]
+	resDto.Data = data
+
+	res, err := json.Marshal(resDto)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal response: %w", err)
+	}
+
+	err = msg.Respond(res)
+
+	if err != nil {
+		return fmt.Errorf("Failed to send error response via nats: %w", err)
+	}
+
+	return nil
+}
