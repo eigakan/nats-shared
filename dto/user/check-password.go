22bcda8892ae18e14c028f860ef7d5bb8bd0bd86
lmm1ng <dei4@yandex.ru>
2025-08-11 15:28:05 +0300

    feat: change contract

--- dto/user/check-password.go
+++ dto/user/check-password.go
@@ -1,10 +1,17 @@
 package dto
 
+import "time"
+
 type CheckPasswordRequestDTO struct {
 	Login    string `json:"login"`
 	Password string `json:"password"`
 }
 
 type CheckPasswordResponseDTO struct {
-	Valid bool `json:"valid"`
+	ID        uint      `json:"id"`
+	Email     string    `json:"email"`
+	Login     string    `json:"login"`
+	Logo      string    `json:"logo"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
